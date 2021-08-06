1fba9f99d0c2c4d90ea726f3fe83def73b79f70f
Vaibhav Kaushik <vaibhavkaushik@Vaibhavs-MacBook-Pro.local>
2021-08-06 16:06:12 +0530

    Expects to not create user with duplicate contact

--- src/test/java/com/example/splitwise/RepositoryTest/UserRepositoryTest.java
+++ src/test/java/com/example/splitwise/RepositoryTest/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.splitwise.RepositoryTest;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.splitwise.Model.User;
@@ -17,9 +18,9 @@ public class UserRepositoryTest {
 
     @Test
     public void expectsToCreateUserInPersistentStorage() {
-        User User1 = userRepository.save(new User("John", "9999999999", "john@123.com"));
+        User user1 = userRepository.save(new User("John", "9999999999", "john@123.com"));
 
-        Optional<User> userFoundById = userRepository.findById(User1.getId());
+        Optional<User> userFoundById = userRepository.findById(user1.getId());
         assertTrue(userFoundById.isPresent());
     }
 
@@ -27,5 +28,16 @@ public class UserRepositoryTest {
     public void expectsToFindUserWithValidID() {
         Optional<User> userFoundById = userRepository.findById(1);
         assertTrue(userFoundById.isPresent());
+    }
+
+    @Test
+    public void expectsToNotCreateDuplicateUserWithSameContact() {
+        User user2 = null;
+        try{
+            user2 = userRepository.save(new User("Jill", "9999999999", "jill@123.com"));
+        }
+        catch (Exception ignored){
+        }
+        assertNull(user2);
     }
 }
